95dba2bcb333165f28d589a76b38508720cd2a7c
thomkeh <thomas5max@gmail.com>
2020-10-09 13:05:21 +0100

    Make the number optional

--- human_readable_ids/random_word.py
+++ human_readable_ids/random_word.py
@@ -4,5 +4,8 @@ from human_readable_ids.nouns import get_nouns
 from human_readable_ids.adjectives import get_adjectives
 
 
-def get_new_id():
-    return f"{random.choice(get_adjectives()).capitalize()} {random.choice(get_nouns()).capitalize()} {random.randrange(1, 100)}"
+def get_new_id(with_number=True):
+    id_ = f"{random.choice(get_adjectives()).capitalize()} {random.choice(get_nouns()).capitalize()}"
+    if with_number:
+        id_ += f" {random.randrange(1, 100)}"
+    return id_
